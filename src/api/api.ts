1d67f690f912a945f0a3d5246370582a29d7e1ce
Artyom K <artem.kavun90@gmail.com>
2025-05-04 15:38:53 +0300

    d3

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,7 +19,7 @@ export const API = {
     //     return await axios.get(`https://test-task-api.allfuneral.com/auth?user=USERNAME`);
     // },
     async v1_get(): Promise<AxiosResponse<V1DataType>> {
-        return await axios.get(`http://localhost:3000/v1`, {
+        return await axios.get(`http://212.23.211.54:3000/v1`, {
             headers: {
 
                 "Content-Type": "application/json"
@@ -27,7 +27,7 @@ export const API = {
         });
     },
     async v1_delete(id: number): Promise<AxiosResponse<V1DataType>> {
-        return await axios.delete(`hhttp://localhost:3000/v1/${id}`, {
+        return await axios.delete(`http://212.23.211.54/v1/${id}`, {
             headers: {
 
                 "Content-Type": "application/json"
